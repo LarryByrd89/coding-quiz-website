674cce45d062895440e35ad0584231fced382cfb
LarryByrd89 <laryn.n99@gmail.com>
2023-09-14 20:09:12 -0400

    reconstruct files

---
+++ questions.js
@@ -0,0 +1,204 @@
+var score = 0;
+var container = document.querySelector("#container");
+var quizContent = document.querySelector("#quizContent");
+var questionTitle = document.querySelector("#qTitle")
+var timer = document.querySelector("#timer");
+var startBtn = document.querySelector("#start");
+
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["javascript", "scripting", "script", "js"],
+        answer: "script"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["body", "head", "head & body"],
+        answer: "head & body"
+    },
+    {
+        title: "The external JavaScript file must contain the <script> tag.",
+        choices: ["true","false"],
+        answer: "false"
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ["if i = 5", "if i = 5 then", "if i ==5", "if(i==5)"],
+        answer: "if(i==5)"
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ["<!--This-->", "//This", "'This'"],
+        answer: "//This"
+    },
+];
+var questionIndex = 0;
+
+var createUl = document.createElement("ul");
+createUl.setAttribute("id", "optionsUl")
+
+var timeInterval = 0;
+var countdown = 75;
+var penalty = 10;
+
+startBtn.addEventListener("click", function() {
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function() {
+            countdown--;
+            timer.textContent = "Time: " + countdown;
+            if (countdown <= 0) {
+                clearInterval(timeInterval);
+                theEnd();
+            }
+        }, 1000);
+    }
+    newQuestion(questionIndex)
+});
+
+
+// New question
+function newQuestion(questionIndex) {
+    quizContent.innerHTML = "";
+    createUl.innerHTML = "";
+    var displayQuestion = document.createElement("h2");
+
+    for (var i = 0; i < questions.length; i++) {
+        displayQuestion.innerHTML = questions[questionIndex].title;
+        var displayChoices = questions[questionIndex].choices;
+        quizContent.appendChild(displayQuestion);
+    }
+    console.log(displayChoices);
+    displayChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.innerHTML += "<button>" + newItem + "</button>";
+        quizContent.appendChild(createUl);
+        createUl.appendChild(listItem);
+        listItem.addEventListener("click", (checkAns));
+    })
+}
+
+var i = 0;
+var newDiv = document.createElement("div");
+var feedback = document.createElement("h3");
+newDiv.setAttribute("id", "newDiv");
+
+
+// Correct or Incorrect Check
+function checkAns(event) {
+        var choice = event.target;
+        quizContent.appendChild(newDiv);
+        newDiv.appendChild(feedback);
+        var next = document.createElement("button");
+        next.setAttribute("id", "nextButton");
+        next.textContent = "Next Question";
+
+
+// Correct answer
+    if (choice.textContent == questions[questionIndex].answer) {
+        score++;
+        feedback.textContent = "You got it!";
+        newDiv.appendChild(feedback);
+        
+        newDiv.appendChild(next);
+        next.addEventListener("click", (movingOn));
+
+
+//Incorrect answer
+    } else {
+        countdown = countdown - penalty;
+        feedback.textContent = "Not Correct...Try again";
+        newDiv.appendChild(feedback);
+    }
+}
+
+
+// Next question
+function movingOn(event) {
+    newDiv.innerHTML = "";
+    questionIndex++;
+    if (questionIndex >= questions.length) {
+        theEnd();
+    } else {
+        newQuestion(questionIndex);
+
+    }
+}
+
+function theEnd() {
+    quizContent.innerHTML = "";
+    timer.innerHTML = "";
+
+
+// High score page
+    var newH1 = document.createElement("h1");
+    newH1.setAttribute("id", "newH1");
+    newH1.textContent = "Finished!"
+    quizContent.appendChild(newH1);
+
+
+// Final score
+    if (countdown >= 0) {
+        score = countdown;
+        clearInterval(timeInterval);
+        var newP = document.createElement("p");
+        newP.textContent = "Your final score is: " + score;
+        quizContent.appendChild(newP);
+    } else {
+        score = 0;
+        var outOfTime = document.createElement("h2");
+        outOfTime.textContent = "Time is up! 🕔";
+        quizContent.appendChild(outOfTime);
+        var newP = document.createElement("p");
+        newP.textContent = "Your final score is: " + score;
+        quizContent.appendChild(newP);
+    }
+
+
+// Initials
+    var initialsPrompt = document.createElement("label");
+    initialsPrompt.setAttribute("for", "inputBox");
+    initialsPrompt.textContent = "Enter your initials: ";
+    quizContent.appendChild(initialsPrompt);
+
+    var inputBox = document.createElement("input");
+    inputBox.setAttribute("type", "text");
+    inputBox.setAttribute("id", "inputBox")  
+    inputBox.textContent = "";
+    quizContent.appendChild(inputBox)
+    
+    var submit = document.createElement("button");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("id", "submit");
+    submit.textContent = "Submit";
+    quizContent.appendChild(submit);
+
+
+// Event listener
+    submit.addEventListener("click", function() {
+        var initials = inputBox.value;
+
+        if (initials === "") {
+            console.log("No initials entered")
+            window.alert("Please enter your initials");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: score
+            }
+
+
+// Previous scores
+            var storeScores = localStorage.getItem("storeScores");
+            if (storeScores === null) {
+                storeScores = [];
+            } else {
+                storeScores = JSON.parse(storeScores);
+            }
+            storeScores.push(finalScore);
+            var newScore = JSON.stringify(storeScores);
+            localStorage.setItem("storeScores", newScore);
+            window.location.replace("highscores.html");
+        }
+    });
+};
